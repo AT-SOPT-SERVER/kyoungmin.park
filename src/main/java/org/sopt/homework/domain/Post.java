04de118aa4e30d6f977822dde2fe3ea8d211725d
kymin <park48664866@gmail.com>
2025-04-24 23:45:36 +0900

    feat($dto): 요청, 응답에 대한 dto 구현
    요청, 응답에 대한 dto 구현:
    - 요청 dto 구현
    - 응답 dto 구현

--- src/main/java/org/sopt/homework/domain/Post.java
+++ src/main/java/org/sopt/homework/domain/Post.java
@@ -1,23 +1,67 @@
 package org.sopt.homework.domain;
 
-import org.sopt.homework.domain.util.validator.Validator;
-import org.sopt.homework.exception.ExceptionMessage;
+import java.time.LocalDateTime;
 
-public class Post {
+import org.hibernate.annotations.CreationTimestamp;
+import org.sopt.homework.domain.util.validator.Validator;
+import org.sopt.homework.global.exception.GlobalException;
+import org.sopt.homework.global.exception.message.ExceptionMessage;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "post")
+public final class Post {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "post_id", nullable = false, unique = true, updatable = false)
+	private Long id;
+
+	// 이모지만 30개 넣으면 varChar 기본 크기인 255바이트가 모자랄 수 있음
+	@Column(name = "title", nullable = false, length = 1024)
 	private String title;
 
+	@CreationTimestamp
+	@Column(name = "created_at", nullable = false, updatable = false)
+	private LocalDateTime createdAt;
+
 	public Post(String title) {
-		if (!Validator.isValidTitle(title)) {
-			throw new IllegalArgumentException(ExceptionMessage.INVALID_TITLE.getMessage());
-		}
+		// 검증 결과에 따라 예외처리
+		validateTitle(title);
 		this.title = title;
+	}
+
+	public Post() {
+	}
+
+	public Long getId() {
+		return this.id;
 	}
 
 	public String getTitle() {
 		return this.title;
 	}
 
+	// 게시글 수정 시 사용되는 update 메서드
 	public void updateTitle(String title) {
+		validateTitle(title);
 		this.title = title;
+	}
+
+	private void validateTitle(String title) {
+		if (title == null) {
+			throw new GlobalException(ExceptionMessage.BAD_REQUEST);
+		}
+		if (Validator.isEmptyTitle(title)) {
+			throw new GlobalException(ExceptionMessage.EMPTY_TITLE);
+		}
+		if (Validator.isOverLengthTitle(title)) {
+			throw new GlobalException((ExceptionMessage.OVER_LENGTH_TITLE));
+		}
 	}
 }
--- src/main/java/org/sopt/homework/domain/util/validator/Validator.java
+++ src/main/java/org/sopt/homework/domain/util/validator/Validator.java
@@ -7,9 +7,12 @@ public class Validator {
 	// 하나의 시각적 유니코드 grapheme의 정규식인 "\X"를 이용하여 문자열의 길이를 카운트
 	private static final Pattern GRAPHEME_PATTERN = Pattern.compile("\\X");
 
-	public static boolean isValidTitle(String title) {
-		// isEmpty는 길이가 0인 경우만 true를 리턴, isBlank는 길이가 0이거나 공백으로만 이루어진 문자열인 경우 true
-		return !title.isBlank() && lengthWithEmoji(title) < 30;
+	public static boolean isEmptyTitle(String title) {
+		return title.isBlank();
+	}
+
+	public static boolean isOverLengthTitle(String title) {
+		return lengthWithEmoji(title) > 30;
 	}
 
 	private static int lengthWithEmoji(String title) {
@@ -19,6 +22,7 @@ public class Validator {
 		while (matcher.find()) {
 			count++;
 		}
+
 		return count;
 	}
 }
