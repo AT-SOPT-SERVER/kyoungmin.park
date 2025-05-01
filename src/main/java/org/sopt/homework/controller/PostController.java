25bbad7ddfaa79cf42e55476fcbaa868317e208a
kymin <park48664866@gmail.com>
2025-05-01 17:02:58 +0900

    refactor: 응답 형태 적용 방법 수정
    응답 형태 적용 방법 수정:
    - 생성, 삭제 기능에서 공통 응답 설정을 위해 response message 반환

--- src/main/java/org/sopt/homework/controller/PostController.java
+++ src/main/java/org/sopt/homework/controller/PostController.java
@@ -1,9 +1,9 @@
 package org.sopt.homework.controller;
 
-import java.util.List;
-
-import org.sopt.homework.dto.PostRequest;
-import org.sopt.homework.dto.PostResponse;
+import org.sopt.homework.dto.post.request.PostWriteRequest;
+import org.sopt.homework.dto.post.response.PostDetailResponse;
+import org.sopt.homework.dto.post.response.PostListResponse;
+import org.sopt.homework.global.api.message.ResponseMessage;
 import org.sopt.homework.service.PostService;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,39 +27,48 @@ public class PostController {
 
 	// 게시글 생성
 	@PostMapping
-	public PostResponse createPost(@RequestBody final PostRequest postRequestDto) {
-		return postService.createPost(postRequestDto);
+	public ResponseMessage createPost(
+		@RequestHeader(name = "userId", required = true) final long userId,
+		@RequestBody final PostWriteRequest postWriteRequestDto) {
+		postService.createPost(userId, postWriteRequestDto);
+		return ResponseMessage.POST_CREATED;
 	}
 
 	// 게시글 조회
 	@GetMapping
-	public List<PostResponse> getPosts() {
+	public PostListResponse getPosts() {
 		// 쿼리스트링으로 키워드가 전달되면 검색을 진행, 키워드가 전달되지 않으면 전체 게시글을 조회
 		return postService.getAllPosts();
 	}
 
-	// 게시글 검색
 	@GetMapping(path = "/search")
-	public List<PostResponse> searchPosts(@RequestParam(value = "keyword") final String keyword) {
-		return postService.getPostsByTitle(keyword);
+	public PostListResponse searchPosts(
+		@RequestParam(name = "author", required = false) final String author,
+		@RequestParam(name = "title", required = false) final String title,
+		@RequestParam(name = "tag", required = false) final String tagName
+	) {
+		return postService.searchPosts(author, title, tagName);
 	}
 
 	// 게시글 상세 조회
 	@GetMapping(path = "/{postId}")
-	public PostResponse getPostById(@PathVariable(value = "postId") final Long id) {
-		return postService.getPostById(id);
+	public PostDetailResponse getPostById(@PathVariable(value = "postId") final long postId) {
+		return postService.getPostById(postId);
 	}
 
-	// 게시글 제목 수정
+	// 게시글 수정
 	@PutMapping(path = "/{postId}")
-	public void updatePostTitle(@PathVariable(value = "postId") final Long id,
-		@RequestBody final PostRequest postRequestDto) {
-		postService.updatePost(id, postRequestDto);
+	public void updatePostTitle(
+		@RequestHeader(name = "userId", required = true) final long userId,
+		@PathVariable(value = "postId") final long postId,
+		@RequestBody final PostWriteRequest postWriteRequestDto) {
+		postService.updatePost(postId, userId, postWriteRequestDto);
 	}
 
 	// 게시글 삭제
 	@DeleteMapping(path = "/{postId}")
-	public void deletePostById(@PathVariable(value = "postId") final Long id) {
-		postService.deletePostById(id);
+	public ResponseMessage deletePostById(@PathVariable(value = "postId") final long postId) {
+		postService.deletePostById(postId);
+		return ResponseMessage.POST_DELETED;
 	}
 }
--- src/main/java/org/sopt/homework/controller/UserController.java
+++ src/main/java/org/sopt/homework/controller/UserController.java
@@ -1,6 +1,7 @@
 package org.sopt.homework.controller;
 
 import org.sopt.homework.dto.user.UserSignupRequest;
+import org.sopt.homework.global.api.message.ResponseMessage;
 import org.sopt.homework.service.UserService;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,12 +20,14 @@ public class UserController {
 	}
 
 	@PostMapping("/signup")
-	public void create(@RequestBody final UserSignupRequest userSignupRequest) {
+	public ResponseMessage create(@RequestBody final UserSignupRequest userSignupRequest) {
 		userService.createUser(userSignupRequest);
+		return ResponseMessage.POST_CREATED;
 	}
 
 	@DeleteMapping
-	public void delete(@RequestHeader(name = "userId", required = true) final long userId) {
+	public ResponseMessage delete(@RequestHeader(name = "userId", required = true) final long userId) {
 		userService.deleteUser(userId);
+		return ResponseMessage.POST_DELETED;
 	}
 }
